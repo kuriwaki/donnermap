9444e26c95d54c5643d6639c2d2b4eadd25050a2
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-02-17 22:40:19 -0500

    Add rep name

--- data-raw/CD-info.R
+++ data-raw/CD-info.R
@@ -1,13 +1,32 @@
 # This loads from an external repo. Contact Shiro Kuriwaki for more
 
 library(tidyverse)
+library(Rvoteview)
+library(glue)
 
 full_cd <- read_rds("~/Dropbox/CCES_representation/data/output/averages/by-cd_info.Rds")
+full_reps <-
+  member_search(chamber = "House", congress = 116) %>%
+  as_tibble()
+
+rep_116 <- full_reps %>%
+  transmute(icpsr,
+            cd = glue("{state_abbrev}-{str_pad(district_code, width = 2, pad = '0')}"),
+            ush116_rep = bioname,
+            ush116_party = str_sub(party_name, 1, 1)) %>%
+  group_by(cd) %>%
+  arrange(desc(icpsr)) %>% # not sure which one's best
+  distinct(cd, .keep_all = TRUE) %>%
+  ungroup() %>%
+  select(-icpsr)
+
 
 cd_info <- full_cd %>%
   mutate(year = 2018) %>%
+  left_join(rep_116, by = "cd") %>%
   select(year, cd,
          dailykos_name = descrip, largest_place = place,
+         matches("ush"),
          pct_trump, pct_romney, pct_mccain)
 
 
