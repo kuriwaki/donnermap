1ccc1dc6aaafe0986fe546600b1b23a1c2035e16
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-02-24 00:03:48 -0500

    Document race numbers

--- R/datadoc.R
+++ R/datadoc.R
@@ -1,5 +1,16 @@
 #' Counts and fractions of race by congressional district
 #'
+#' @details Counts are taken from the 5-year ACS.
+#'  ACS codes used are `"B01001_001"` for Total VAP population,
+#'  `"B01001H_001"` for Non-Hispanic White,
+#'  `"B01001B_001"` for Black,
+#'  `"B01001I_001"` for Hispanic,
+#'  `"B01001D_001"` for Asian, and
+#'  `"B01001C_001"` for American Indian.
+#'
+#'
+#' @seealso <https://www.shirokuriwaki.com/ccesMRPprep/reference/get_acs_cces.html>
+#'
 #' @author 5-Year American Community Survey, 2014-2018
 "race_by_cd"
 
