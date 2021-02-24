e5905e8133f034985207d7dccda62e27b3111fbd
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-02-24 00:00:49 -0500

    Document and cite CD info

--- R/datadoc.R
+++ R/datadoc.R
@@ -1,5 +1,17 @@
 #' Counts and fractions of race by congressional district
+#'
+#' @author 5-Year American Community Survey, 2014-2018
 "race_by_cd"
 
+#' District 2018 Descriptions and Information
+#'
+#' @author Daily Kos Elections
+#'
+#' @source
+#'   The Daily Kos Elections naming guide to the nation's congressional districts.
+#'   \url{https://bit.ly/2XsFI5W}
+#'
+#'   Daily Kos, "2008, 2012, & 2016 results for districts used in 2018."
+#'   \url{https://bit.ly/3bXtAPB}
 #' @seealso <https://www.shirokuriwaki.com/ccesMRPprep/reference/cd_info_2018.html>
 "cd_info"
